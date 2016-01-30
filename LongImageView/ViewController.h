2930164fdbd56862377f237b73ecdf431257d5f8
GD1207 <1023251767@qq.com>
2016-01-30 16:29:31 +0800

    Initial Commit

---
+++ LongImageView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LongImageView
+//
+//  Created by xiaoyu on 16/1/30.
+//  Copyright © 2016年 Guoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LongImageView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LongImageView
+//
+//  Created by xiaoyu on 16/1/30.
+//  Copyright © 2016年 Guoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
